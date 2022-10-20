51fe3cfe8295d6063ccb36e62d28d27fa6d8264b
Jan Eisen <jan.eisen@tutanota.de>
2022-10-20 18:40:02 +0000

    Feature: When you click 'Start playing' the difficulty settings are hidden and the playing field shown

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,17 @@ function userChoseDifficulty(event) {
     columns = document.getElementById('height').value;
     mines = document.getElementById('mines').value;
     console.log(`User wants a playing field that's ${rows} x ${columns} with ${mines} mines.`);
+    // Hide the settings menu:
+    const difficultySettings = document.getElementById('difficulty-settings');
+    difficultySettings.style.display = 'none';
+    // Show and style playing field:
+    const playingField = document.getElementById('playing-field');
+    playingField.style.display = 'grid';
+    playingField.style.alignContent = 'center';
+}
+
+function calculateMaxNumberOfMines() {
+    console.log('YO');
 }
 
 form = document.getElementById('set-difficulty');
