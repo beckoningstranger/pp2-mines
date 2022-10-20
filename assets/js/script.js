e1751481105ebce7e13c7667235ec458c32bfc4a
Jan Eisen <jan.eisen@tutanota.de>
2022-10-20 20:04:52 +0000

    Feature: An array of objects is created based on the parameters the user set. This array will be fed more information and will then act as the backbone of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
-function userChoseDifficulty(event) {
+/**
+ * Reads how big the playing field is supposed to be and how many mines should be placed from the DOM.
+ * When user clicks 'Start Playing!' it hides the menu where the user could set game parameters and calls functions to build the playing field.
+ */
+function startGame(event) {
     event.preventDefault();
-    rows = document.getElementById('width').value;
-    columns = document.getElementById('height').value;
-    mines = document.getElementById('mines').value;
-    console.log(`User wants a playing field that's ${rows} x ${columns} with ${mines} mines.`);
+    let width = document.getElementById('width').value;
+    let height = document.getElementById('height').value;
+    let mines = document.getElementById('mines').value;
+    console.log(`User wants a playing field that's ${width} x ${height} with ${mines} mines.`);
     // Hide the settings menu:
     const difficultySettings = document.getElementById('difficulty-settings');
     difficultySettings.style.display = 'none';
@@ -11,11 +15,46 @@ function userChoseDifficulty(event) {
     const playingField = document.getElementById('playing-field');
     playingField.style.display = 'grid';
     playingField.style.alignContent = 'center';
+    buildPlayingField(width, height, mines)
 }
 
-function calculateMaxNumberOfMines() {
-    console.log('YO');
+/**
+* Generates an array of objects called "squares". Each object is a square on the playing field and contains the
+* following information: 
+* 1. Its name, 
+* 2. Whether it has a mine, 
+* 3. Whether the player has already clicked it
+* 4. How many mines there are in adjacent squares
+*/
+function buildPlayingField(width, height, mines) {
+    const LETTERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    let rows = [];
+    let columns = [];
+
+    // Create an array of letters for rows based on how many the user chose
+    for (let i = 0; i < width; i++) {
+        rows.push(LETTERS[i]);
+    }
+    // Create an array of numbers for columns based on how many the user chose
+    for (let i = 0; i < height; i++) {
+        columns.push(NUMBERS[i]);
+    }
+    console.log(rows, columns, mines);
+    let squares = [];
+    for (let a = 0; a < rows.length; a++) {
+        for (let b = 1; b < columns.length + 1; b++) {
+                let square = {
+                    name: rows[a] + b,
+                    hasMine: 0,
+                    uncovered: 0,
+                    minesNextDoor: 0,
+            }
+        squares.push(square);
+        }
+    }
+    console.log(squares);
 }
 
 form = document.getElementById('set-difficulty');
-form.addEventListener('submit', userChoseDifficulty);
+form.addEventListener('submit', startGame);
