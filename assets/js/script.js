7acc12eea034f2dd632e9a8c8797a51a6690e69f
Jan Eisen <jan.eisen@tutanota.de>
2022-10-20 14:05:02 +0000

    Form submission is now handled by JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log('Connected!');
+function userChoseDifficulty(event) {
+    event.preventDefault();
+    rows = document.getElementById('width').value;
+    columns = document.getElementById('height').value;
+    mines = document.getElementById('mines').value;
+    console.log(`User wants a playing field that's ${rows} x ${columns} with ${mines} mines.`);
+}
+
+form = document.getElementById('set-difficulty');
+form.addEventListener('submit', userChoseDifficulty);
