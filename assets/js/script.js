6c3b2e306a241d73b34b39ec867441487144431a
Jan Eisen <jan.eisen@tutanota.de>
2022-10-21 22:53:00 +0000

    Chore: Add comments to code to make it more readable. Feature: Game now tracks how many squares user marked as mines Fix: Tracking how many squares the user clicked was broken, as user could simply click the same square until he won. This is now fixed by only counting squares that have the unrevealed class and then removing this class from them

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ form = document.getElementById('set-difficulty');
 form.addEventListener('submit', startGame);
 var squaresToWin;
 var clickedSquares = 0;
+var foundMines = 0;
 
 /**
  * Reads how big the playing field is supposed to be and how many mines should be placed from the DOM.
@@ -33,9 +34,6 @@ var clickedSquares = 0;
 
     // Add event listeners to each created button
     makePlayingFieldInteractive();
-
-
-
 }
 
 /**
@@ -104,7 +102,7 @@ function layMines(minesToLay, playingField) {
  */
 function findSurroundingSquares(squares, rows, columns) {
     for (let i = 0; i < squares.length; i++) {
-    
+
         let borderSquareHorizontally = 0;
   	    let borderSquareVertically = 0;
 	    let letterBefore = '';
@@ -131,12 +129,16 @@ function findSurroundingSquares(squares, rows, columns) {
                 letterBefore = rows[lettersIndexNumber - 1];
                 letterAfter = rows[lettersIndexNumber + 1];
             }
-            
+        
+        // If this square is on the left border, i.e a1, b1, etc., make a note of that (this will cause the next if statement to return 'false')
+        // and set numberAfter accordingly
         if (numberOfThisSquare === 1) {
             numberAfter = 2;
             borderSquareVertically = 1;
         }
         
+        // If this square is on the right border, set numberBefore accordingly
+        // If it's not on either border, set numberBefore and numberAfter
         if ((numberOfThisSquare === columns.length) && (borderSquareVertically === 0)) {
             numberBefore = columns.length - 1;
         } else {
@@ -145,11 +147,19 @@ function findSurroundingSquares(squares, rows, columns) {
                 numberAfter =  numberOfThisSquare + 1;
             }
         }
-	    if (numberBefore === 0 ) {
+
+        // This if statement makes sure corner squares don't have a0 etc. as neighboring squares.
+	    if (numberBefore === 0) {
             numberBefore = '';
 	    }
-        let adjacentSquares = [letterOfThisSquare + numberAfter, letterOfThisSquare + numberBefore, letterBefore + numberOfThisSquare, letterBefore + numberBefore, letterBefore + numberAfter, letterAfter + numberOfThisSquare, letterAfter + numberBefore, letterAfter + numberAfter];
-	    // Clean up the generated squares above by iterating in reverse order through the array and splicing all from the array that are either undefined, NaN or just a letter or a number, not a combination of both.
+
+        // Now it puts the letters and numbers together to form square names like d3, e5 or a2. Where there are values missing, 
+        // it will produce broken square names like "a" or "3". These would be squares that we won't want anyway, so it's a good thing.
+        let adjacentSquares = [letterOfThisSquare + numberAfter, letterOfThisSquare + numberBefore, letterBefore + numberOfThisSquare, 
+            letterBefore + numberBefore, letterBefore + numberAfter, letterAfter + numberOfThisSquare, letterAfter + numberBefore, letterAfter + numberAfter];
+
+	    // Clean up broken square names generated above by iterating in reverse order through the array and splicing all from the array 
+        // that are either undefined, NaN or just a letter or a number, not a combination of both.
 	    for (let i = adjacentSquares.length - 1; i >= 0; i--) {
             if ((adjacentSquares[i].length < 2) || (adjacentSquares[i] === "undefined") || (Number.isNaN(adjacentSquares[i]))) {
                 let indexOfThatSquare = adjacentSquares.indexOf(adjacentSquares[i]);
@@ -232,8 +242,11 @@ function makePlayingFieldInteractive() {
             if (this.classList.contains('has-mine')) {
                 console.log('GAME OVER');
             } else {
-                clickedSquares++;
-                console.log(`${clickedSquares}/${squaresToWin}`);
+                if (this.classList.contains('unrevealed')) {
+                    clickedSquares++;
+                    console.log(`${clickedSquares}/${squaresToWin}`);
+                    this.classList.remove('unrevealed');
+                }
                 if (clickedSquares === squaresToWin) {
                     console.log('YOU WIN!')
                 }
@@ -276,9 +289,13 @@ function makePlayingFieldInteractive() {
             switch (this.style.backgroundColor) {
                 case "":
                     this.style.backgroundColor = "black";
+                    foundMines++;
+                    console.log(`Looks like you found ${foundMines} mines.`)
                     break;
                 case "black":
                     this.style.backgroundColor = "";
+                    foundMines--;
+                    console.log(`Looks like you found ${foundMines} mines.`)
                     break;
             }
             // this.innerText = `<i class='fa-solid fa-land-mine-on'></i>`;
